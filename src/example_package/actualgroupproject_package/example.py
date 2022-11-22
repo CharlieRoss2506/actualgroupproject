4cd3ff834c5d856acc3a4e6170221b0ab2176955
charlotteharrison1 <116881713+charlotteharrison1@users.noreply.github.com>
2022-11-22 16:19:38 +0000

    added our own installation package

---
+++ src/example_package/actualgroupproject_package/example.py
@@ -0,0 +1,8 @@
+# run code to input potential in command line
+
+import solving_TDSE
+
+
+solve_TDSE()
+#should return "Please input potential using Nummpy format" in the command line. Input a valid potential to return an animation of the wavefunction over time for a particle trapped in a finite square well of that potential.
+
