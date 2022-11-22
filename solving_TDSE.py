13b1a55e78b592926f7279d3e16f1aa78817f5aa
charlotteharrison1 <116881713+charlotteharrison1@users.noreply.github.com>
2022-11-22 16:03:10 +0000

    made the code a callable function.
    ideally upon import when the user types solve_TDSE the code will run

--- solving_TDSE.py
+++ solving_TDSE.py
@@ -14,151 +14,152 @@ from scipy.sparse import eye, diags
 import matplotlib.animation as animation
 
 
-# Inputting parameters, defining space & time variables
-Nx = 250
-xmin = -5
-xmax = 5
-Nt = 100
-tmin = 0
-tmax = 60
+def solve_TDSE:
+    # Inputting parameters, defining space & time variables
+    Nx = 250
+    xmin = -5
+    xmax = 5
+    Nt = 100
+    tmin = 0
+    tmax = 60
 
-'''
-READ BEFORE INPUTTING POTENTIALS
-When inputting own potentials use the variable x_array
-Numpy packages can be used to form a string as the potential
-'''
+    '''
+    READ BEFORE INPUTTING POTENTIALS
+    When inputting own potentials use the variable x_array
+    Numpy packages can be used to form a string as the potential
+    '''
 
-# Calculating grid, potential, and initial wave function
-x_array = np.linspace(xmin, xmax, Nx)
-t_array = np.linspace(tmin, tmax, Nt)
-
-
-'''
-Defining a function to be returned as the default potential 
-'''
-def f(x_array):
-    return x_array**2
+    # Calculating grid, potential, and initial wave function
+    x_array = np.linspace(xmin, xmax, Nx)
+    t_array = np.linspace(tmin, tmax, Nt)
 
 
-def V():
-    String = str(input("Please input potential\n"))
-    command = """def f(x):
-          return """ + String
-
-    exec(command, globals())
-
-    try: 
-        return f(x_array)
-    except: # add exceptions for errors from program execution.
-        print("\nV(x) input does not compute.")
-        print("Please input a valid mathematical equation in Numpy format terms of x_array.")
-        print("\nSome examples:")
-        print("... x_array**2")
-        print("... np.sin(x_array)")
-        print("\nPlease see the documentation for more details")
-        print("\nClosing programme...\n")
-        exit()
-
-VV = V()
+    '''
+    Defining a function to be returned as the default potential 
+    '''
+    def f(x_array):
+        return x_array**2
 
 
-'''
-Converting V into a Diagonal matrix and calculating small psi
-'''
-Vmatrix = diags(VV)
-psi = np.exp(-(x_array+2)**2)
+    def V():
+        String = str(input("Please input potential\n"))
+        command = """def f(x):
+              return """ + String
 
-'''
-Calculating finite difference elements
-'''
-dt = t_array[1] - t_array[0]
-dx = x_array[1] - x_array[0]
+        exec(command, globals())
 
-'''
-Calculating the Hamiltonian matrix
-'''
-H = -0.5 * FinDiff(0, dx, 2).matrix(x_array.shape) + Vmatrix 
-'''
-Note that FinDiff above is a way of representing our partial derivatives. 
-FinDiff objects behave like operators; they have a tuple of the form (axis, spacing, degree) in the argument list for each partial derivative.
-The last argument stands for the degree of the derivative (in our case 2).
-'''
+        try: 
+            return f(x_array)
+        except: # add exceptions for errors from program execution.
+            print("\nV(x) input does not compute.")
+            print("Please input a valid mathematical equation in Numpy format terms of x_array.")
+            print("\nSome examples:")
+            print("... x_array**2")
+            print("... np.sin(x_array)")
+            print("\nPlease see the documentation for more details")
+            print("\nClosing programme...\n")
+            exit()
 
-'''
-Applying boundary conditions to the Hamiltonian
-'''
-H[0, :] = H[-1, :] = 0
-H[0, 0] = H[-1, -1] = 1
-
-'''
-Calculating matrix U, the discretized version of the time propagation operator in the Crank-Nicholson scheme
-'''
-I_plus = eye(Nx) + 1j * dt / 2. * H # eye returns all elements to 0 except diagonal to 1.
-I_minus = eye(Nx) - 1j * dt / 2. * H
-U = inv(I_minus).dot(I_plus) 
-
-'''
-Iterating over each time, appending each calculation of psi to make a list of all the calculations
-'''
-psi_list = []
-for t in t_array:
-    psi = U.dot(psi)
-    psi[0] = psi[-1] = 0
-    psi_list.append(np.abs(psi))
+    VV = V()
 
 
-    
-    while True:
-        data_input = input("\nWould you like to download a csv file of the data? (y/n)")
-        if data_input == 'y':
+    '''
+    Converting V into a Diagonal matrix and calculating small psi
+    '''
+    Vmatrix = diags(VV)
+    psi = np.exp(-(x_array+2)**2)
 
-            df = pd.DataFrame({'x': x, 'psi': psi_list})
-            df.to_csv('psi_data.csv')
-            print("\nData has been downloaded.")
-            break
-        elif data_input ==  'n':
-            print("\nNo download chosen. ")
-            break   
-        else:
-            print("\nInvalid response.")
-            print("Please enter... y (yes for download) or n (no download).")
+    '''
+    Calculating finite difference elements
+    '''
+    dt = t_array[1] - t_array[0]
+    dx = x_array[1] - x_array[0]
 
-download_data(Nx, xmin, xmax, tmin, tmax, V()==VV)   
-    
-    
-"""
-We now have a numerical solution of simulated data (i.e. `psi_list`) saved to our disk.
-In order to visualize this, we plot the data in 2D
-"""
+    '''
+    Calculating the Hamiltonian matrix
+    '''
+    H = -0.5 * FinDiff(0, dx, 2).matrix(x_array.shape) + Vmatrix 
+    '''
+    Note that FinDiff above is a way of representing our partial derivatives. 
+    FinDiff objects behave like operators; they have a tuple of the form (axis, spacing, degree) in the argument list for each partial derivative.
+    The last argument stands for the degree of the derivative (in our case 2).
+    '''
 
-fig, ax = plt.subplots()
+    '''
+    Applying boundary conditions to the Hamiltonian
+    '''
+    H[0, :] = H[-1, :] = 0
+    H[0, 0] = H[-1, -1] = 1
 
-ax.set_xlabel("x [arb units]")
-ax.set_ylabel("$|\Psi(x, t)|$", color="C0")
+    '''
+    Calculating matrix U, the discretized version of the time propagation operator in the Crank-Nicholson scheme
+    '''
+    I_plus = eye(Nx) + 1j * dt / 2. * H # eye returns all elements to 0 except diagonal to 1.
+    I_minus = eye(Nx) - 1j * dt / 2. * H
+    U = inv(I_minus).dot(I_plus) 
 
-ax_twin = ax.twinx()
-ax_twin.plot(x_array, VV, color="C1") # where time increases vertically on the y-axis. Note the probability density moves back and forth
-ax_twin.set_ylabel("V(x) [arb units]", color="C1")
+    '''
+    Iterating over each time, appending each calculation of psi to make a list of all the calculations
+    '''
+    psi_list = []
+    for t in t_array:
+        psi = U.dot(psi)
+        psi[0] = psi[-1] = 0
+        psi_list.append(np.abs(psi))
 
-line, = ax.plot([], [], color="C0", lw=2)
-ax.grid()
-xdata, ydata = [], []
 
-def run(psi):
-    line.set_data(x_array, np.abs(psi)**2)
-    return line,
 
-ax.set_xlim(x_array[0], x_array[-1])
-ax.set_ylim(0, 1)
+        while True:
+            data_input = input("\nWould you like to download a csv file of the data? (y/n)")
+            if data_input == 'y':
 
-'''
-A better, more cohesive way of viewing the data is with an animation
-'''
-ani = animation.FuncAnimation(fig, run, psi_list, interval=10)
-ani.save("particle_in_a_well.mp4", fps=120, dpi=300) # The code will be saved as an mp4 file
+                df = pd.DataFrame({'x': x, 'psi': psi_list})
+                df.to_csv('psi_data.csv')
+                print("\nData has been downloaded.")
+                break
+            elif data_input ==  'n':
+                print("\nNo download chosen. ")
+                break   
+            else:
+                print("\nInvalid response.")
+                print("Please enter... y (yes for download) or n (no download).")
 
-plt.show()
+    download_data(Nx, xmin, xmax, tmin, tmax, V()==VV)   
 
-'''
-Hence our animation shows the evolution of the probability density function with time, inputting simulated data previously created.
-'''
+
+    """
+    We now have a numerical solution of simulated data (i.e. `psi_list`) saved to our disk.
+    In order to visualize this, we plot the data in 2D
+    """
+
+    fig, ax = plt.subplots()
+
+    ax.set_xlabel("x [arb units]")
+    ax.set_ylabel("$|\Psi(x, t)|$", color="C0")
+
+    ax_twin = ax.twinx()
+    ax_twin.plot(x_array, VV, color="C1") # where time increases vertically on the y-axis. Note the probability density moves back and forth
+    ax_twin.set_ylabel("V(x) [arb units]", color="C1")
+
+    line, = ax.plot([], [], color="C0", lw=2)
+    ax.grid()
+    xdata, ydata = [], []
+
+    def run(psi):
+        line.set_data(x_array, np.abs(psi)**2)
+        return line,
+
+    ax.set_xlim(x_array[0], x_array[-1])
+    ax.set_ylim(0, 1)
+
+    '''
+    A better, more cohesive way of viewing the data is with an animation
+    '''
+    ani = animation.FuncAnimation(fig, run, psi_list, interval=10)
+    ani.save("particle_in_a_well.mp4", fps=120, dpi=300) # The code will be saved as an mp4 file
+
+    plt.show()
+
+    '''
+    Hence our animation shows the evolution of the probability density function with time, inputting simulated data previously created.
+    '''
